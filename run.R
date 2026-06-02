b5e81f8379a5293761539d93936050315e7c7106
Swati <arcturexx@gmail.com>
2026-06-02 19:46:34 +0530

    Fix run.R path: point to /app not /srv/shiny-server

--- run.R
+++ run.R
@@ -1,2 +1,2 @@
 port <- as.integer(Sys.getenv("PORT", "7860"))
-shiny::runApp("/srv/shiny-server", host = "0.0.0.0", port = port, launch.browser = FALSE)
+shiny::runApp("/app", host = "0.0.0.0", port = port, launch.browser = FALSE)
